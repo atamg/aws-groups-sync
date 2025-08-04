e1b03c2b3f77297d0515e84c223644dead9d2b7e
Ata Mahmoudi <ata.mahmoudi@gmail.com>
2025-08-04 17:26:08 +0200

    Add config module to configure settings for the scrip

---
+++ src/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    AD_SERVER = os.getenv("AD_SERVER")
+    AD_USER = os.getenv("AD_USER")
+    AD_PASSWORD = os.getenv("AD_PASSWORD")
+    AD_BASE_DN = os.getenv("AD_BASE_DN")
+    AD_GROUP_FILTER = os.getenv("AD_GROUP_FILTER", "AWS_*")
